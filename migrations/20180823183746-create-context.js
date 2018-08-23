88618710fdfef709712837b17707356d72032d39
Dedek Julianto <dedekproject@gmail.com>
2018-08-24 03:30:02 +0700

    migrations associations user,context,task

--- migrations/20180823183746-create-context.js
+++ migrations/20180823183746-create-context.js
@@ -1,6 +1,8 @@
 'use strict';
 module.exports = {
+  // function to run when (trying) to create the table (or make needed changes to it)
   up: (queryInterface, Sequelize) => {
+    // define all columns in Contexts, including id, createdAt, and updatedAt as well as foreign keys (see UserId)
     return queryInterface.createTable('Contexts', {
       id: {
         allowNull: false,
@@ -11,6 +13,14 @@ module.exports = {
       },
       name: {
         type: Sequelize.STRING
+      },
+      UserId: {
+        type: Sequelize.UUID,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Users',
+          key: 'id'
+        }
       },
       createdAt: {
         allowNull: false,
@@ -29,6 +39,7 @@ module.exports = {
       return queryInterface.createTable('users', { id: Sequelize.INTEGER });
     */
   },
+  // function to run when reverting the changes to the table
   down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable('Contexts');
     /*
--- migrations/20180823183833-create-task.js
+++ migrations/20180823183833-create-task.js
@@ -1,6 +1,8 @@
 'use strict';
 module.exports = {
+  // function to run when (trying) to create the table (or make needed changes to it)
   up: (queryInterface, Sequelize) => {
+    // define all columns in Tasks, including id, createdAt, and updatedAt as well as foreign keys (see ContextId)
     return queryInterface.createTable('Tasks', {
       id: {
         allowNull: false,
@@ -19,6 +21,14 @@ module.exports = {
       description: {
         type: Sequelize.TEXT
       },
+      ContextId: {
+        type: Sequelize.UUID,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Contexts',
+          key: 'id'
+        }
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
@@ -36,6 +46,7 @@ module.exports = {
       return queryInterface.createTable('users', { id: Sequelize.INTEGER });
     */
   },
+  // function to run when reverting the changes to the table
   down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable('Tasks');
     /*
